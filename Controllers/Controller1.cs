697ba306f8c877ec4d720ae38e9be0dbebf2ade9
diana-stephani-moscu <dianamoscu19@gmail.com>
2023-10-19 22:54:45 +0300

    Inclus cele 3 metode

---
+++ Controllers/Controller1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moscu_Diana_Stephani_Lab1.Controllers
+{
+    public class Controller1 : Controller
+    {
+        public string Index()
+        {
+            return "Bine ati venit!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+        public string MesajCuParametri(string text, int numar)
+        {
+            return $"Textul primit: {text}, Numarul primit: {numar}";
+        }
+    }
+}
